a092eac399d0f4a6874344f26554e38161319c82
Wagner Meneses <wagnermeneses89@gmail.com>
2025-12-31 00:02:45 -0300

    Delete Elemento.ts

--- Elemento.ts
+++
@@ -1,9 +0,0 @@
-export class Elemento{
-    nome: string;
-    prioridade: number;
-
-    constructor(identificacao: string, nivelPrioridade: number){
-        this.nome = identificacao;
-        this.prioridade = nivelPrioridade;
-    }  
-}
